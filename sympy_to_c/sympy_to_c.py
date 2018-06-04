7c8cdd97b247975a0cf1c7c3bd77c03a36d8589d
Carsten Knoll <carsten.knoll@posteo.de>
2018-06-04 21:53:38 +0200

    first working version

--- sympy_to_c/sympy_to_c.py
+++ sympy_to_c/sympy_to_c.py
@@ -1,3 +1,178 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
-"""Main module."""
+import ctypes as ct
+
+import inspect
+import os
+import numpy as np
+import itertools as it
+import sympy as sp
+from sympy.utilities.codegen import codegen
+
+from ipydex import IPS  # just for debugging
+
+CLEANUP = True
+
+
+def path_of_caller(*paths):
+    frm = inspect.stack()[2]
+    mod = inspect.getmodule(frm[0])
+    res = os.path.dirname(mod.__file__)
+    return res
+
+
+def _get_c_func_name(base, i, j):
+    return "{}_{}_{}".format(base, i, j)
+
+
+def compile_ccode(cfileath):
+
+    assert cfileath.endswith(".c")
+    objfilepath = "{}.o".format(cfileath[:-2])
+    sofilepath = "{}.so".format(cfileath[:-2])
+    cmd1 = "gcc -c -fPIC -lm {}".format(cfileath)
+    cmd2 = "gcc -shared {} -o {}".format(objfilepath, sofilepath)
+
+    print(cmd1)
+    os.system(cmd1)
+    print("\n\n{}".format(cmd2))
+    os.system(cmd2)
+
+    if CLEANUP:
+        os.remove(cfileath)
+        os.remove(objfilepath)
+
+    return sofilepath
+
+
+def convert_to_c(args, expr, basename="expr", cfilepath="sp2clib.c", pathprefix=None,
+                 use_exisiting_so=True):
+    """
+
+    :param args:
+    :param expr:
+    :param basename:
+    :param cfilepath:
+    :param use_exisiting_so:    Omits generation of c-code if an .so-file with appropriate name
+                                already exists.
+
+    :return:    python-callable wrapping the respective c-functions
+    """
+
+    if pathprefix is None:
+        pathprefix = path_of_caller()
+    assert isinstance(pathprefix, basestring)
+
+    cfilepath = os.path.join(pathprefix, cfilepath)
+
+    assert cfilepath.endswith(".c")
+    sopath = "{}.so".format(cfilepath[:-2])
+    if isinstance(expr, sp.MatrixBase):
+        shape = expr.shape
+        expr_matrix = expr
+        scalar_flag = False
+    else:
+        scalar_flag = True
+        shape = (1, 1)
+        expr_matrix = sp.Matrix([expr])
+
+    if not use_exisiting_so or not os.path.isfile(sopath):
+        _generate_ccode(args, expr_matrix, basename, cfilepath, shape)
+
+        sopath = compile_ccode(cfilepath)
+
+    if scalar_flag:
+        funcname = _get_c_func_name(basename, 0, 0)
+        return load_func_from_solib(sopath, funcname, len(args))
+    else:
+        return load_matrix_func_from_solib(sopath, basename, expr_matrix.shape, len(args))
+
+
+def _generate_ccode(args, expr_matrix, basename, libname, shape):
+
+    nr, nc = shape
+    # list of index-pairs
+    idcs = it.product(range(nr), range(nc))
+
+    ccode_list = []
+    for i, j in idcs:
+        tmp_expr = expr_matrix[i, j]
+
+        partfuncname = _get_c_func_name(basename, i, j)
+
+        c_res = codegen( (partfuncname, tmp_expr), "C", "test",
+                         header=False, empty=False, argument_sequence=args)
+        [(c_name, ccode), (h_name, c_header)] = c_res
+
+        ccode = "\n".join(line for line in ccode.split("\n") if not line.startswith("#include"))
+
+        ccode_list.append(ccode)
+
+    res = "\n\n".join(ccode_list)
+
+    final_code = "#include <math.h>\n\n{}".format(res)
+
+    with open(libname, "w") as cfile:
+        cfile.write(final_code)
+
+
+def load_func_from_solib(libpath, funcname, nargs):
+    """
+
+    :param libname:
+    :param funcname:
+    :param nargs:       number of float args
+    :return:
+    """
+
+    # ensure that the path prefix is at least "./"
+    prefix, name = os.path.split(libpath)
+    if prefix == "":
+        libpath = os.path.join(".", libpath)
+    lib = ct.cdll.LoadLibrary(libpath)
+
+    # TODO: throw exception on failure
+    the_c_func = getattr(lib, funcname)
+
+    # this converts the result in a python float obj:
+    the_c_func.restype = ct.c_double
+    the_c_func.argtypes = [ct.c_double]*nargs
+
+    # the caller must take care of the number of args
+    def thefunc(*args):
+        assert len(args) == nargs
+
+        res = the_c_func(*args)
+
+        return res
+
+    return thefunc
+
+
+# noinspection PyPep8Naming
+def load_matrix_func_from_solib(libname, basename, shape, nargs):
+    """
+
+    :param libname:
+    :param basename:
+    :param shape:
+    :param nargs:
+    :return:
+    """
+
+    nr, nc = shape
+
+    # list of index-pairs
+    idcs = it.product(range(nr), range(nc))
+
+    M_func_list = []
+    for i, j in idcs:
+        funcname = _get_c_func_name(basename, i, j)
+        M_func_list.append(load_func_from_solib(libname, funcname, nargs))
+
+    def M_func(*args):
+        assert len(args) == nargs
+        return np.r_[ [f(*args) for f in M_func_list] ].reshape(shape)
+
+    return M_func
--- tests/test_sympy_to_c.py
+++ tests/test_sympy_to_c.py
@@ -5,18 +5,56 @@
 
 
 import unittest
+import sympy as sp
+import numpy as np
+import os
 
-from sympy_to_c import sympy_to_c
+from ipydex import IPS
+
+from sympy_to_c import sympy_to_c as sp2c
 
 
+# noinspection PyPep8Naming, PyTypeChecker
 class TestSympy_to_c(unittest.TestCase):
     """Tests for `sympy_to_c` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        x1, x2, x3 = self.xx = sp.symbols("x1, x2, x3")
+
+        self.e1 = x1*x2 + x3
+
+        self.M1 = sp.Matrix([self.e1, sp.sin(x1)*sp.exp(x3)])
+
+        np.random.seed(1840)
+        N = 100
+        self.XX = np.random.random((N, len(self.xx)))
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        os.system("rm *.so")
 
-    def test_000_something(self):
+    def test_scalar_expression(self):
         """Test something."""
+
+        e1_c_func = sp2c.convert_to_c(self.xx, self.e1, cfilepath="scalar.c")
+        e1_l_func = sp.lambdify(self.xx, self.e1)
+
+        for xx in self.XX:
+            self.assertAlmostEqual(e1_c_func(*xx), e1_l_func(*xx))
+
+    def test_matrix_expression(self):
+        """Test something."""
+
+        M1_c_func = sp2c.convert_to_c(self.xx, self.M1, cfilepath="matrix.c")
+        M1_l_func = sp.lambdify(self.xx, self.M1)
+
+        for xx in self.XX:
+            self.assertTrue(np.allclose(M1_c_func(*xx), M1_l_func(*xx)))
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
