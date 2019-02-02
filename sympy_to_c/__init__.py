3bc6c27a441535b4ff1e2284648ddd8cbf8d4322
Carsten Knoll <carsten.knoll@posteo.de>
2019-02-02 17:10:53 +0100

    support leagacy import scheme

--- sympy_to_c/__init__.py
+++ sympy_to_c/__init__.py
@@ -5,4 +5,17 @@
 from .core import convert_to_c, get_meta_data, unload_lib, unload_all_libs, created_so_files,\
     reproducible_fast_hash
 
-__version__ = '0.1.1'
+
+# support leagacy imports like from sympy_to_c import sympy_to_c as sp2c
+class Container(object):
+    pass
+
+sympy_to_c = Container()
+sympy_to_c.convert_to_c = convert_to_c
+sympy_to_c.get_meta_data = get_meta_data
+sympy_to_c.unload_lib = unload_lib
+sympy_to_c.unload_all_libs = unload_all_libs
+sympy_to_c.created_so_files = created_so_files
+sympy_to_c.reproducible_fast_hash = reproducible_fast_hash
+
+__version__ = '0.1.2'
