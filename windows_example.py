fce3046ea98f821d37c9be3228d6a5b12eb2aa8d
Robert Heedt <robert.heedt@gmail.com>
2019-08-04 00:57:31 +0200

    Add Numba to benchmark
    Only run the Numba parts, if it is installed
    Use random arguments for timing
    Use NumPy arguments for timing (lambdify performs better)

--- windows_example.py
+++ windows_example.py
@@ -1,7 +1,14 @@
 import sympy as sp
 import sympy_to_c as sp2c
+import numpy as np
 import os
 import timeit
+try:
+    # Run benchmark with Numba, if it is installed
+    from numba import jit
+    numba_loaded = True
+except ImportError:
+    numba_loaded = False
 
 # Add GCC to the PATH environment variable. Modify this with your MinGW installation directory.
 os.environ['PATH'] += os.pathsep + r"[YOUR_MINGW_DIR]\mingw64\bin"
@@ -25,16 +32,41 @@ def series_subs(x_, x0_):
 print("Lambdifying...")
 series_lambdify = sp.lambdify([x, x0], series_expr, 'numpy')
 
+if numba_loaded:
+    print("Building Numba function...")
+    series_numba = jit(series_lambdify, nopython=True)
+
 print("Compiling sp2c function...")
-series_sp2c = sp2c.convert_to_c([x, x0], series_expr, cfilepath="example.c", use_exisiting_so=False)
+series_sp2c = sp2c.convert_to_c([x, x0], series_expr, cfilepath="example.c")
+
+print("Warming up different variants...")
+# Generate random arguments to be used for all variants
+# At least lambdify performs better with NumPy float64 arguments, so keep that in mind for a fair comparison
+repeats = 100
+np.random.seed(0+118+999+881+999+119+725+3)
+x_args = np.random.rand(repeats)
+x0_args = np.random.rand(repeats)
+
+# The first run might take a lot longer than the following ones, so we might want to "warm up" by executing each once
+# Be careful to use the same argument types here as in the timeit command (Numba compiles for specific types)
+value_subs = series_subs(x_args[0], x0_args[0])
+value_lambdify = series_lambdify(x_args[0], x0_args[0])
+if numba_loaded:
+    value_numba = series_numba(x_args[0], x0_args[0])
+value_sp2c = series_sp2c(x_args[0], x0_args[0])
 
 print("Timing different variants...")
-time_subs = timeit.timeit("series_subs(1, 0)", globals=globals(), number=10000)
-time_lambdify = timeit.timeit("series_lambdify(1, 0)", globals=globals(), number=10000)
-time_sp2c = timeit.timeit("series_sp2c(1, 0)", globals=globals(), number=10000)
+# Use a counter 'i' to use a different random argument for each repeat, but the same arguments across the variants
+time_subs = timeit.timeit("series_subs(x_args[i], x0_args[i]); i += 1", setup="i=0", globals=globals(), number=repeats)
+time_lambdify = timeit.timeit("series_lambdify(x_args[i], x0_args[i]); i += 1", setup="i=0", globals=globals(), number=repeats)
+if numba_loaded:
+    time_numba = timeit.timeit("series_numba(x_args[i], x0_args[i]); i += 1", setup="i=0", globals=globals(), number=repeats)
+time_sp2c = timeit.timeit("series_sp2c(x_args[i], x0_args[i]); i += 1", setup="i=0", globals=globals(), number=repeats)
 
 print("--- Results ---")
-print(f"Accurate value with subs: {float(expr.subs([(x, 1), (x0, 0)]))}")
-print(f"Series value with subs: {series_subs(1, 0)}, Time: {time_subs}")
-print(f"Series value with lambdify: {series_lambdify(1, 0)}, Time: {time_lambdify}")
-print(f"Series value with sp2c: {series_sp2c(1, 0)}, Time: {time_sp2c}")
+print(f"Accurate value with subs: {float(expr.subs([(x, x_args[0]), (x0, x0_args[0])]))}")
+print(f"Series value with subs: {value_subs}, Time: {time_subs}")
+print(f"Series value with lambdify: {value_lambdify}, Time: {time_lambdify}")
+if numba_loaded:
+    print(f"Series value with Numba: {value_numba}, Time: {time_numba}")
+print(f"Series value with sp2c: {value_sp2c}, Time: {time_sp2c}")
