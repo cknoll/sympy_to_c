deec6b2b5285cd9c5a994681bba19fd6b1c6734c
Carsten Knoll <carsten.knoll@posteo.de>
2024-08-21 16:24:35 +0200

    release.py: bump version to 0.2.0: all tests pass

--- sympy_to_c/__init__.py
+++ sympy_to_c/__init__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for sympy to c."""
 
 from .core import convert_to_c, get_meta_data, unload_lib, unload_all_libs, created_so_files,\
     reproducible_fast_hash
 
+from .release import __version__
 
 # support leagacy imports like from sympy_to_c import sympy_to_c as sp2c
 class Container(object):
@@ -17,5 +16,3 @@ sympy_to_c.unload_lib = unload_lib
 sympy_to_c.unload_all_libs = unload_all_libs
 sympy_to_c.created_so_files = created_so_files
 sympy_to_c.reproducible_fast_hash = reproducible_fast_hash
-
-__version__ = '0.1.2'
